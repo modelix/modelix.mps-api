31b0a27f2cd0297e053417047c4c50f1dde3989f
Sascha Lisson <sl@q60.de>
2025-01-26 19:34:22 +0100

    build: check for binary compatibility

--- api/src/main/kotlin/org/modelix/mps/api/IModelixMpsApi.kt
+++ api/src/main/kotlin/org/modelix/mps/api/IModelixMpsApi.kt
@@ -1,15 +1,15 @@
 package org.modelix.mps.api
 
-import com.intellij.openapi.application.ApplicationInfo
 import jetbrains.mps.project.Project
 import org.jetbrains.mps.openapi.module.SModule
 
 interface IModelixMpsApi {
     fun getMPSProjects(): List<Project>
     fun getVirtualFolder(project: Project, module: SModule): String?
+    fun fixVersions(project: Project, module: SModule)
+
     fun getVirtualFolders(module: SModule): List<String> = getMPSProjects().mapNotNull {
         getVirtualFolder(it, module).takeIf { !it.isNullOrEmpty() }
     }
-
-    fun fixVersions(project: Project, module: SModule)
+    fun getVirtualFolder(module: SModule): String? = getVirtualFolders(module).firstOrNull()
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.shadow)
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.binaryCompatibility)
     `maven-publish`
 }
 
