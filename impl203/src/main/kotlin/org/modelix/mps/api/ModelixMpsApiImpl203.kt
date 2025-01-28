8d53c404a3bb8c5bece73cbca368fff2316b9231
Sascha Lisson <sl@q60.de>
2025-01-28 10:57:56 +0100

    fix: use correct `Project` type from openapi

--- impl203/src/main/kotlin/org/modelix/mps/api/ModelixMpsApiImpl203.kt
+++ impl203/src/main/kotlin/org/modelix/mps/api/ModelixMpsApiImpl203.kt
@@ -2,9 +2,9 @@ package org.modelix.mps.api
 
 import jetbrains.mps.lang.migration.runtime.base.VersionFixer
 import jetbrains.mps.project.MPSProject
-import jetbrains.mps.project.Project
 import jetbrains.mps.project.ProjectManager
 import org.jetbrains.mps.openapi.module.SModule
+import org.jetbrains.mps.openapi.project.Project
 
 open class ModelixMpsApiImpl203 : IModelixMpsApi {
     override fun getMPSProjects(): List<Project> {
@@ -19,6 +19,6 @@ open class ModelixMpsApiImpl203 : IModelixMpsApi {
     }
 
     override fun fixVersions(project: Project, module: SModule) {
-        VersionFixer(project, module, true).updateImportVersions()
+        VersionFixer(project as jetbrains.mps.project.Project, module, true).updateImportVersions()
     }
 }
