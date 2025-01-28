3897e14a214c6234289f33de712da58afd6958db
Sascha Lisson <sl@q60.de>
2025-01-28 11:20:20 +0100

    chore: stop using ProjectManager.getInstance starting with 223

--- impl223/src/main/kotlin/org/modelix/mps/api/ModelixMpsApiImpl223.kt
+++ impl223/src/main/kotlin/org/modelix/mps/api/ModelixMpsApiImpl223.kt
@@ -1,3 +1,12 @@
 package org.modelix.mps.api
 
-open class ModelixMpsApiImpl223 : ModelixMpsApiImpl222()
+import jetbrains.mps.ide.MPSCoreComponents
+import jetbrains.mps.project.ProjectManager
+import org.jetbrains.mps.openapi.project.Project
+
+open class ModelixMpsApiImpl223 : ModelixMpsApiImpl222() {
+    override fun getMPSProjects(): List<Project> {
+        val manager = MPSCoreComponents.getInstance()?.platform?.findComponent(ProjectManager::class.java) ?: return emptyList()
+        return manager.openedProjects
+    }
+}
